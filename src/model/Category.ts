ef79d50c00783421cb60f9f1522a84d46ebe7c6d
Wilian Calora <wiliancalora@gmail.com>
2021-10-14 07:27:11 -0300

    Separação para camada de repository

---
+++ src/model/Category.ts
@@ -0,0 +1,17 @@
+import { constructor } from "express";
+import { v4 as uuidV4 } from "uuid";
+
+class Category {
+  id?: string;
+  name: string;
+  description: string;
+  create_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Category };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,28 @@
 import { response, Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+
+import { CategoryRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories = [];
+const categoriesRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    name,
-    description,
-    id: uuidV4(),
-  };
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push(category);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already exists!" });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
