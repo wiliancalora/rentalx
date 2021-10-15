5068d6c0b3316611d35967f9654543becc37210c
Wilian Calora <wiliancalora@gmail.com>
2021-10-15 07:54:20 -0300

    Criando os Use Case de Categoria

--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -27,6 +27,8 @@ class SpecificationsRepository implements ISpecificationsRepository {
     const specification = this.specifications.find(
       (specification) => specification.name === name
     );
+
+    return specification;
   }
 }
 
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 // import { PostgresCategoriesRepository } from "../repositories/PostgresCategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 
@@ -11,13 +11,7 @@ const categoriesRepository = new CategoriesRepository();
 // const categoriesRepository = new PostgresCategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
